d1cf2ec7718dfb99d0772b0ccb3c3533d5301e48
Vign <7vign7@gmail.com>
2024-05-31 00:50:26 +0300

    CollDark)))

--- main.js
+++ main.js
@@ -2,22 +2,33 @@ const light = document.querySelector("#light")
 const dark =  document.querySelector(`#dark`)
 const button = document.querySelector(`button`)
 const styleMode = localStorage.getItem(`styleMode`)
+const paths = document.querySelectorAll("svg > path")
+const test = document.querySelector(".didj__logo > svg")
 button.addEventListener("click", darkAge)
 function darkAge() {
     document.body.classList.toggle(`darkBody`)
     if (document.body.className === `darkBody`){
+        for (let i = 0; i < paths.length; i++ ){
+            paths[i].setAttribute('fill','white')
+        }
         light.classList.remove('mama')
         dark.classList.add('mama')
         localStorage.setItem(`styleMode`,`dark`)
     }
     else{
+        for (let i = 0; i < paths.length; i++ ){
+            paths[i].setAttribute('fill','black')
+        }
         dark.classList.remove('mama')
         light.classList.add('mama')
         localStorage.setItem(`styleMode`,null)
     }
 }
+const oop = "#7FFFD4"
+test.style.filter = `drop-shadow(0 0 25px ${oop}`
 if (styleMode === `dark`){
     darkAge()
+
 }
 
 
